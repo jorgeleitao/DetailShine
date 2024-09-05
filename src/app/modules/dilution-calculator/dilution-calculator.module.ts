d575732ff0c6730347873b104a2e7acab3112d14
Jorge Leitão <jorgeleitao08@gmail.com>
2024-09-05 15:29:34 -0300

    Declaring components

--- src/app/modules/dilution-calculator/dilution-calculator.module.ts
+++ src/app/modules/dilution-calculator/dilution-calculator.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DilutioncalculatorComponent } from './dilutioncalculator.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DilutioncalculatorComponent,
+  ],
   imports: [
     CommonModule
   ]
--- src/app/modules/produc-list/produc-list.module.ts
+++ src/app/modules/produc-list/produc-list.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductlistComponent } from './productlist.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProductlistComponent,
+  ],
   imports: [
     CommonModule
   ]
