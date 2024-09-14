e4525fef8ed3f45fb788a98376b83325369aaa54
Jorge Leitão <jorgeleitao08@gmail.com>
2024-09-14 17:19:38 -0300

    commit-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,21 @@
 import { ListProductComponent } from './modules/list-product/list-product.component';
 
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { DilutioncalculatorComponent } from './modules/dilution-calculator/dilutioncalculator.component';
 import { CustcalculatorComponent } from './modules/cust-calculator/custcalculator.component';
+import { BodyComponent } from './components/body/body.component';
 
 const routes: Routes = [
+  {
+    path : '',
+    redirectTo: 'body',
+    pathMatch: 'full'
+  },
+
   { path : 'header',
     component: HeaderComponent,
-    pathMatch: 'full'
   },
 
   {
@@ -23,7 +29,12 @@ const routes: Routes = [
   {
     path: 'cust-calculator',
     component: CustcalculatorComponent
+  },
+  {
+    path: 'body',
+    component: BodyComponent
   }
+
 ];
 
 
